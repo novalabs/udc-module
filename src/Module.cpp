9b61e334c3e3e03ac7b8cb444e4241c5b9d1b0c1
Davide Rizzi <d.rizzi@novalabs.io>
2016-06-16 16:16:49 +0200

    Formatted

--- src/Module.cpp
+++ src/Module.cpp
@@ -3,7 +3,7 @@
  * All rights reserved. All use of this software and documentation is
  * subject to the License Agreement located in the file LICENSE.
  */
- 
+
 #include <Core/MW/Middleware.hpp>
 
 #include "ch.h"
